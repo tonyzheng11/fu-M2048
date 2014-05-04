a186945f395843b47f7a9acbcd7bc13c65f467b4
Tony Zheng <tony.zheng@Administrators-MacBook-Air.local>
2014-05-03 22:52:04 -0400

    Changed mobile link

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -137,7 +137,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "You win!" : "You suck Monique!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
